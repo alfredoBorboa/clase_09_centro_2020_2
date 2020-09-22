32b90f28780f3ddf2b3ce20e68289e9bc4a6afc4
Alfredo Borboa Osorio <alfredoborboaosorio@MacBook-Pro-16-inch-de-Alfredo.local>
2020-09-22 13:09:11 -0500

    Strobo

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready( function(){
+  setInterval( function(){
+    $( "body" ).css( 'background-color', '#F00' );
+    setTimeout( function(){
+      $( "body" ).css( 'background-color', '#00F' );
+    }, 100 );
+  }, 200);
+});
